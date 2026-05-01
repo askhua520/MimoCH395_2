456de02c7025412250c4e7a39ddd6a0731ebe76a
askhua520 <fanghaiyu@gmail.com>
2026-05-01 22:22:48 +0800

    add App/Web/web_resources.h

---
+++ App/Web/web_resources.h
@@ -0,0 +1,10 @@
+#ifndef __WEB_RESOURCES_H
+#define __WEB_RESOURCES_H
+
+#include <stdint.h>
+
+/* Embedded HTML page */
+extern const char web_index_html[];
+extern const uint32_t web_index_html_len;
+
+#endif /* __WEB_RESOURCES_H */
