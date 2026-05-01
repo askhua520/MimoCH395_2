83c47884c377b386e65d2b8ed8659ae61f543a83
askhua520 <fanghaiyu@gmail.com>
2026-05-01 22:36:06 +0800

    add main.h

---
+++ Inc/main.h
@@ -0,0 +1,12 @@
+/**
+  * @file    main.h
+  * @brief   Main program header
+  */
+#ifndef __MAIN_H
+#define __MAIN_H
+
+#include "stm32f1xx_hal.h"
+
+void Error_Handler(void);
+
+#endif /* __MAIN_H */
